64507cab2d1aaeb7f939eee4431714103817ecd8
humonnom <josojoso0702@gmail.com>
2022-05-29 12:37:50 +0900

    update gitignore file to ignore test files and remove few more lines from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
+import { css } from "@emotion/react";
 import { useMemo } from "react";
 import { Container } from "../layouts/Layout";
 export default function Home() {
   const Contents = useMemo(() => {
     return (
       <>
-        <div>
-        </div>
+        <h2 css={PageTitleStyle} >.Intro</h2>
+        <p>메인 페이지 준비중</p>
       </>
     );
   }, []);
   return <Container contents={Contents} />;
 }
+
+const PageTitleStyle = css`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+`;
