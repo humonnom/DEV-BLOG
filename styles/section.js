3a2469cc4e69d410943afbb0631f0b69d4f00759
humonnom <josojoso0702@gmail.com>
2022-07-13 22:05:27 +0900

    섹션의 너비 제한을 줌

--- styles/section.js
+++ styles/section.js
@@ -4,6 +4,7 @@ import { BORDER_STYLE, COLOR_STYLE, FlexCenter, FONT_SIZE, mq } from "./global";
 export const SectionStyle = css`
   ${FlexCenter}
   height: 100%;
+  width: 350px;
   padding: 70px 10px 100px 10px;
   margin: 0px 0px;
   border: ${BORDER_STYLE.white};
